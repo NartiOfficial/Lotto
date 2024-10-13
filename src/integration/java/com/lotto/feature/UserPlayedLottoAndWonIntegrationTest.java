27f8688d38650bd9f12d5d5191d47852964336c2
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2024-10-13 13:29:56 +0200

    Added overrided bean for adjustableClock

--- src/integration/java/com/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
+++ src/integration/java/com/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -36,7 +36,7 @@ public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
     //  Step 2: System fetched winning numbers for draw date: 12.10.2024 12:00
 
         // Given
-        LocalDateTime drawDate = LocalDateTime.of(2024, 10,12,0,0);
+        LocalDateTime drawDate = LocalDateTime.of(2024, 10, 12, 12, 0, 0);
 
         // When && Then
         await()
@@ -53,6 +53,7 @@ public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
 
     //  Step 3: User made POST /inputNumbers with 6 numbers (1, 2, 3, 4, 5, 6) at 16-11-2022 10:00 and system returned OK(200) with message: “success” and Ticket (DrawDate:19.11.2022 12:00 (Saturday), TicketId: sampleTicketId)
     //  Step 4: 3 days and 1 minute passed, and it is 1 minute after the draw date (19.11.2022 12:01)
+        clock.plusDaysAndMinutes(3, 1);
     //  Step 5: System generated result for TicketId: sampleTicketId with draw date 19.11.2022 12:00, and saved it with 6 hits
     //  Step 6: 3 hours passed, and it is 1 minute after announcement time (19.11.2022 15:01)
     //  Step 7: User made GET /results/sampleTicketId and system returned 200 (OK)
